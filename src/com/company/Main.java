9cca66ab652d46b0559129c4c6b9b314dc93c1f3
Volkan20 <volkandereli1@gmail.com>
2022-05-15 11:27:21 -0400

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         BasicFeatures car = new BasicFeatures();
         System.out.println("volakn");
+        System.out.println("dereli");
     }
 }
